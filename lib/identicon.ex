333e6adbd5d21b8b050f85d3fb87b4b1d8030111
Marcle Rodrigues <marcle.rodrigues@codeminer42.com>
2017-01-29 12:49:15 -0300

    Add build grid function and create grid property at Image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,22 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
